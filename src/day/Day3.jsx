245759af4f9092d8e32212dc943da1b136990b79
mrpickles3rd <mrpickles3rd@googlemail.com>
2018-12-22 14:08:47 +0000

    linting

--- src/day/Day3.jsx
+++ src/day/Day3.jsx
@@ -2,17 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Day3({ input, input2, name, name2, handleInputChange }) {
-  // NOTE: Not handling zeros (0), just added one or two to the result.
-  function getStartingSize(rows) {
-    // This is a bit OTT and way to big for what I need.
-    const xy = rows
-      .map(a => [(a[0] + a[2] + 2), (a[1] + a[3] + 2)])
-      .reduce((a, b) => [(a[0] + b[0]), (a[1] + b[1])], [0, 0]);
-
-    const grid = Array.from({ length: xy[1] }, () => Array.from({ length: xy[0] }, () => 0));
-
-    return grid;
-  }
   const values = input.split('\n').map(
     v => v
       .replace(/(\s+|,|:|x|#|@)/g, ' ')
@@ -21,8 +10,8 @@ function Day3({ input, input2, name, name2, handleInputChange }) {
       .slice(1, 5)
       .map(v2 => parseInt(v2, 10)),
   );
-  const grid = Array.from({ length: 1234 }, () => Array.from({ length: 1234 }, () => 0)); // getStartingSize(values);
-console.log('values === ', values)
+  const grid = Array.from({ length: 1234 }, () => Array.from({ length: 1234 }, () => 0));
+
   values.forEach((v, idx) => {
     for (let i = 0; i < v[2]; i += 1) { // ToDo: Check the v[n] x/y value
       for (let j = 0; j < v[3]; j += 1) { // ToDo: Check the v[n] x/y value
