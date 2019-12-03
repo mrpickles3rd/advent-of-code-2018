404b3e4e3fd52367fbe59014765688b5b0ef737f
mrpickles3rd <mrpickles3rd@googlemail.com>
2019-12-03 19:20:43 +0000

    holy crap it looks like it works!

--- src/days2019/Day2.test.jsx
+++ src/days2019/Day2.test.jsx
@@ -48,17 +48,32 @@ describe('<Day2 />', () => {
   });
 
   describe('edges cases;', () => {
-/*
+    it('1,0,0,0,99 becomes 2,0,0,0,99 (1 + 1 = 2)', () => {
+      const testInput = '1,0,0,0,99';
+      const output = '2,0,0,0,99';
+      const comp = shallow(<Day2 input={testInput} />);
+      expect(comp.find('#output').text()).toBe(output);
+    });
 
-    1,0,0,0,99 becomes 2,0,0,0,99 (1 + 1 = 2).
-    2,3,0,3,99 becomes 2,3,0,6,99 (3 * 2 = 6).
-    2,4,4,5,99,0 becomes 2,4,4,5,99,9801 (99 * 99 = 9801).
-    1,1,1,4,99,5,6,0,99 becomes 30,1,1,4,2,5,6,0,99.
+    it('2,3,0,3,99 becomes 2,3,0,6,99 (3 * 2 = 6)', () => {
+      const testInput = '2,3,0,3,99';
+      const output = '2,3,0,6,99';
+      const comp = shallow(<Day2 input={testInput} />);
+      expect(comp.find('#output').text()).toBe(output);
+    });
 
-*/
-    it('1,0,0,0,99 becomes 2,0,0,0,99 (1 + 1 = 2)');
-    it('2,3,0,3,99 becomes 2,3,0,6,99 (3 * 2 = 6)');
-    it('2,4,4,5,99,0 becomes 2,4,4,5,99,9801 (99 * 99 = 9801)');
-    it('1,1,1,4,99,5,6,0,99 becomes 30,1,1,4,2,5,6,0,99');
+    it('2,4,4,5,99,0 becomes 2,4,4,5,99,9801 (99 * 99 = 9801)', () => {
+      const testInput = '2,4,4,5,99,0';
+      const output = '2,4,4,5,99,9801';
+      const comp = shallow(<Day2 input={testInput} />);
+      expect(comp.find('#output').text()).toBe(output);
+    });
+
+    it('1,1,1,4,99,5,6,0,99 becomes 30,1,1,4,2,5,6,0,99', () => {
+      const testInput = '1,1,1,4,99,5,6,0,99';
+      const output = '30,1,1,4,2,5,6,0,99';
+      const comp = shallow(<Day2 input={testInput} />);
+      expect(comp.find('#output').text()).toBe(output);
+    });
   });
 });
